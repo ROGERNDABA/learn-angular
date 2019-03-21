708ea780c2aa66d1d923ebcfe4b9d03965d9482c
rogerndaba <rogerndaba@gmail.com>
2019-03-21 12:45:04 +0200

    adsadasdasda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,19 @@ import { Observable } from "rxjs";
 })
 export class AppComponent {
   items2: Observable<any>;
+  activeMenu: string = "";
 
   constructor(fs: AngularFirestore) {
-    this.items2 = fs.collection("items").valueChanges();
-    this.items2.subscribe(value => {
-      console.log(value);
-    });
+    // this.items2 = fs.collection("items").valueChanges();
+    // this.items2.subscribe(value => {
+    //   console.log(value);
+    // });
+  }
+
+  OpenCloseMenu(menuState: string) {
+    console.log(menuState);
+
+    if (menuState == "Open") this.activeMenu = "activeMenu";
+    else this.activeMenu = "";
   }
 }
